2b236957ad5941e8452bc9739717994aa1da327f
André Hopf <andre.hopf@gmail.com>
2022-12-14 18:19:18 -0300

    Add pipe e component capitalize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapitalizePhillipsPipe } from './pipes/capitalize-phillips.pipe';
+import { CapitalizeComponent } from './components/capitalize/capitalize.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizePhillipsPipe,
+    CapitalizeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
